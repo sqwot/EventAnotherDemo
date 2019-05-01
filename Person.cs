122a45296fe838276e74f6e77722675beb5d9630
sqwot <sqwotina@gmail.com>
2019-05-01 15:28:55 +0300

    Еще немного про события

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace EventAnotherDemo {
+
+    class Person {
+        public event Action GoToSleep = () => { };
+        public event EventHandler DoWork = (x,y) => { };
+        public string Name { get; set; }
+        public Person(string name) {
+            this.Name = name;
+        }
+        public void TakeTime(DateTime now) {
+            if(now.Hour <= 8) {
+                GoToSleep.Invoke();
+            }else {
+                var args = new EventArgs();
+                DoWork.Invoke(this, args);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventAnotherDemo {
+
     class Program {
-        static void Main(string[] args) {
+        public delegate void MyDelegate();
+        public event MyDelegate Event;
+        public event Action EventAction;
+        static void Main() {
+            Person person = new Person("Vasya");
+            person.GoToSleep += Person_GoToSleep;
+            person.DoWork += Person_DoWork;
+
+            person.TakeTime(DateTime.Parse("01.05.2019 21:13:01"));
+            person.TakeTime(DateTime.Parse("01.05.2019 04:13:01"));
+
+
+            Console.ReadKey();
+        }
+
+        private static void Person_DoWork(object sender, EventArgs e) {
+            var prsn = (Person)sender;
+            Console.WriteLine($"Hello, i am {prsn.Name}, and i go to my love work");
+        }
+
+        private static void Person_GoToSleep() {
+            Console.WriteLine("I am go to sleep");
         }
     }
+
 }
